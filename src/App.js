a560c3d823d7fe806067d12db3e89c2762116442
chopdadeep <deep3254c@gmail.com>
2025-06-09 08:36:30 +0530

    frfr

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Routes ,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Login from './Login';
 import Register from './Register';
 import Blog from './Blog';
 import BlogDetails from './BlogDetails';
+import ProtectedRoute from './ProtectedRoute'; // 👈 Import
+
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path='/' element={<Login />}/>
-          <Route path='/blog' element={<Blog />}/>
-          <Route path='/blog/:id' element={<BlogDetails />}/>
-          <Route path='/register' element={<Register />}/>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+
+        {/* Protected Routes */}
+        <Route
+          path='/blog'
+          element={
+            <ProtectedRoute>
+              <Blog />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/blog/:id'
+          element={
+            <ProtectedRoute>
+              <BlogDetails />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </div>
   );
 }
